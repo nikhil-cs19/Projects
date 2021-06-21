6232424a9264d97de1f644fa600d0f9ce1610a6d
nikhil-cs19 <nikhilsn.cs19@bmsce.ac.in>
2021-06-22 00:09:53 +0530

    Added filtring options in admin page

--- ProjectWork20-21/app.js
+++ ProjectWork20-21/app.js
@@ -200,3 +200,43 @@ app.get('/admin',(req,res)=>{
     res.render("admin",{items:values});
   });
 })
+
+app.post('/admin',(req,res)=>{
+  let branch = req.body.branch;
+  let year = req.body.year;
+  console.log(year);
+  console.log(branch);
+  if (branch=="" && year==""){
+    let sql = `select s.FirstName,s.LastName,s.Email, s.student_id,a.branch from signup s, Academic_Details a where s.student_id = a.student_id;`
+    db.query(sql,(err,values)=>{
+    if (err) throw err;
+    res.render("admin",{items:values});
+  });
+  }
+  else if(branch==""){
+    year = year.slice(2,4);
+    console.log(year);
+    let sql = `select s.FirstName,s.LastName,s.Email, s.student_id,a.branch from signup s, Academic_Details a where s.student_id = a.student_id and s.Email like "%${year}%";`
+    db.query(sql,(err,values)=>{
+    if (err) throw err;
+    res.render("admin",{items:values});
+  });
+  }
+  else if(year==""){
+    let sql = `select s.FirstName,s.LastName,s.Email, s.student_id,a.branch from signup s, Academic_Details a where s.student_id = a.student_id and a.branch='${branch}';`
+    db.query(sql,(err,values)=>{
+    if (err) throw err;
+    res.render("admin",{items:values});
+  });
+  }
+  else if(year!="" && branch!=""){
+    year = year.slice(2,4);
+    console.log("debugging output");
+    console.log(year);
+    let sql = `select s.FirstName,s.LastName,s.Email, s.student_id,a.branch from signup s, Academic_Details a where s.student_id = a.student_id and (a.branch='${branch}' and s.email like "19");`
+    db.query(sql,(err,values)=>{
+    if (err) throw err;
+    res.render("admin",{items:values});
+  });
+  }
+})
